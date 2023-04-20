e53f1c23f207eaf2e72324fa7990c7290ef4de46
Jfreyer99 <66331634+Jfreyer99@users.noreply.github.com>
2023-04-20 22:03:50 +0200

    added logout through deleting token

--- routes/User/getUser.js
+++ routes/User/getUser.js
@@ -4,13 +4,13 @@ const router = express.Router();
 router.get('/login-success', (req, res ,next) => {
     if(req.isAuthenticated())
     {
-        return res.send(req.user);
+        return res.send("Success");
     }
     res.status(403).send("Whatcha doing here?")
 });
 
 router.get('/v1/user/isAuth', (req, res, next) => {
-    if(req.isAuthenticated){
+    if(req.isAuthenticated()){
         const username = req.user.username;
         const email = req.user.email;
         return res.status(200).send({username, email});
@@ -23,11 +23,11 @@ router.get("/login-failure", (req, res ,next) => {
 });
 
 router.get("/v1/user/logout", (req, res, next) => {
-    if(req.isAuthenticated){
-        req.logOut();
-        res.send("Logged Out")
+    if(req.isAuthenticated()){
+        res.clearCookie('connect.sid', {path: '/'}).status(200).send('Logged Out');
+        return;
     }
-    res.status(403).send("Error")
+    return res.status(403).send("Error")
 });
 
 module.exports = router;
